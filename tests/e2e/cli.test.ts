41f1884927bf27ee77465efa1fe641d9c5524ec6
Alexandre Marques <pt.alexandremarques@gmail.com>
2024-08-02 10:43:42 +0100

    chore: fix e2e test

--- tests/e2e/cli.test.ts
+++ tests/e2e/cli.test.ts
@@ -2,6 +2,9 @@ import { expect, test } from "vitest";
 import { execSync } from "child_process";
 
 test("CLI app should run", () => {
+	process.env.INSTAGRAM_ACCESS_TOKEN = "test123123";
+	process.env.INSTAGRAM_BUSINESS_ACCOUNT_ID = "123124";
+
 	const output = execSync("node ./dist/index.js").toString();
 	expect(output).toMatch(/Gramoco CLI v\d+\.\d+\.\d+/);
 });
